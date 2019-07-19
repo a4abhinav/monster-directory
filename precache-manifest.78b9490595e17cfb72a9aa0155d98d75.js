46a37b72f42f582986bde9c9bdb70e718c7cb819
a4abhinav <a4abhinavkr87@gmail.com>
2019-07-20 01:25:28 +0530

    Updates

---
+++ precache-manifest.78b9490595e17cfb72a9aa0155d98d75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f034e67732016c1a5f1aad1715d6e1b0",
+    "url": "/monster-directory/index.html"
+  },
+  {
+    "revision": "aaeb6f91f0f711eb02e3",
+    "url": "/monster-directory/static/css/main.fd456887.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-directory/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "aaeb6f91f0f711eb02e3",
+    "url": "/monster-directory/static/js/main.8cf28db0.chunk.js"
+  },
+  {
+    "revision": "bcf499b1b1b98296feae",
+    "url": "/monster-directory/static/js/runtime~main.2e05905e.js"
+  }
+]);
